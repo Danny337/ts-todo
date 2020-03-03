e5e842fc5ba3db550a99dfb3855b4a2d7a41f419
Daniel <dmd17734@gmail.com>
2020-03-03 16:52:25 +0300

    link nav

--- src/components/Navbar.tsx
+++ src/components/Navbar.tsx
@@ -6,8 +6,8 @@ export const Navbar: React.FC = () => {
         <div className="nav-wrapper purple darken-2 px1">
             <a href="/" className="brand-logo">React + TypeScript</a>
             <ul id="nav-mobile" className="right hide-on-med-and-down">
-                <li><a href="sass.html">Список дел</a></li>
-                <li><a href="badges.html">Информация</a></li>
+                <li><a href="#">Список дел</a></li>
+                <li><a href="#">Информация</a></li>
             </ul>
         </div>
     </nav>
